c2d0487dc42dab12936955c4c3b250eb1c14fcb6
Erik <eeckee@gmail.com>
2019-03-13 15:13:34 +0100

    encoding reading json

--- ml_reusable/utils/__init__.py
+++ ml_reusable/utils/__init__.py
@@ -2,9 +2,19 @@ import json
 import csv
 
 
-def read_csv(path, delimiter=','):
+def write_txt(txt, name):
+    """
+    Argument:
+        txt:    list of strings
+        name:   filename
+    """
+    with open(name, "w") as f:
+        f.write("\n".join(txt))
+
+
+def read_csv(path, delimiter=","):
     data = []
-    with open(path, 'r') as f:
+    with open(path, "r") as f:
         csv_reader = csv.reader(f, delimiter=delimiter)
         for row in csv_reader:
             data.append(row)
@@ -12,12 +22,12 @@ def read_csv(path, delimiter=','):
 
 
 def read_json(path):
-    with open(path, 'r') as f:
+    with open(path, "r", encoding="utf8") as f:
         data = json.loads(f.read())
     return data
 
 
 def write_json(data, filename):
-    with open(filename, 'w', encoding='utf-8') as jsonfile:
+    with open(filename, "w", encoding="utf-8") as jsonfile:
         data = json.dump(data, jsonfile, ensure_ascii=False)
     return data
